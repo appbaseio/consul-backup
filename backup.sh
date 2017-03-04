563a61e69580e21c326c1fd0ff33f93124e8dc71
Avi Aryan <avi.aryan123@gmail.com>
2017-03-04 16:50:48 +0530

    chore: take S3 bucket name from environment

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 /usr/bin/consulate --api-host $SERVER_HOST --api-port $SERVER_PORT --token=$CONSUL_TOKEN kv backup --file=/data/kv.json
 /usr/bin/consulate --api-host $SERVER_HOST --api-port $SERVER_PORT --token=$CONSUL_TOKEN acl backup --file=/data/acl.json
-/usr/bin/aws s3 cp /data s3://consul-autopilot-backups/data --recursive
+/usr/bin/aws s3 cp /data s3://$S3_BUCKET_NAME/data --recursive
